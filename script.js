06e3a9d68cb3226e9f4501bf023d732f6df97bee
Tales Cardoso <105746645+talesbruno@users.noreply.github.com>
2025-03-20 21:43:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.faq-question').forEach(question => {
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling;
+    const icon = question.querySelector('.expand-icon');
+
+    // Alterna a visibilidade da resposta
+    answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+
+    // Alterna o ícone de expansão
+    if (answer.style.display === 'block') {
+      icon.src = 'img/iconexp.svg';  // Ícone para "menos"
+    } else {
+      icon.src = 'img/iconexp.svg';  // Ícone para "mais"
+    }
+  });
+});
